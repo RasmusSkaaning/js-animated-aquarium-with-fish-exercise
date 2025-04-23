3f258015e34f110cbc5d0eafefcb178eb5f72b94
RasmusSkaaning <eaa24rlsk@students.eaaa.dk>
2025-04-23 14:38:09 +0200

    3.4 part 2

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 "use strict";
 document.addEventListener("DOMContentLoaded", function () {
-    // All JavaScript goes inside here
+    const anchor = document.querySelector(".anchor");
+    const anchorTooltip = document.getElementById("anchor-tooltip");
+    const audio = document.querySelector("audio"); // All JavaScript goes inside here
   });
